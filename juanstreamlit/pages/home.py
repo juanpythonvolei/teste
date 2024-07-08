2bde5a93a611862daa9a50211377e5ffd13b8924
juanpythonvolei <juanzsalca@outlook.com>
2024-07-07 23:50:06 -0300

    Add files via upload

---
+++ juanstreamlit/pages/home.py
@@ -0,0 +1,226 @@
+import streamlit as st
+import requests
+import re
+from googletrans import Translator
+import folium
+import tkinter as tk    
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+from pathlib import Path
+import os
+import tkcalendar 
+from datetime import datetime
+import xmltodict
+import shutil
+import datetime
+
+# URL da API Directions
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+from geopy.geocoders import Nominatim
+from time import sleep
+import pprint
+from st_circular_progress import CircularProgress
+datas = []
+ref = db.reference('bancodedadosroteirooficial')
+page = st.sidebar.selectbox("Escolha uma página", ["Home", "Login", "Rotas e suporte"])
+css = """
+<style>
+.centered-image {
+    display: block;
+    margin: 0 auto;
+}
+</style>
+"""
+
+# Insere o CSS no aplicativo
+st.markdown(css, unsafe_allow_html=True)
+
+# Exibe a imagem centralizada
+st.image('https://calscycle.ca/newsite/wp-content/uploads/2022/05/Thule.png', width=500)
+start_date = datetime.date(2000, 1, 1)
+end_date = datetime.date(2100, 1, 11)
+lista_filtrada = []
+current_date = start_date
+while current_date <= end_date:
+    current_date += datetime.timedelta(days=1)
+    datas.append(current_date)
+opcao_selecionada = st.selectbox("Selecione uma data", datas)
+
+uploaded_files = st.file_uploader("Escolha vários arquivos", type=['xml'], accept_multiple_files=True)
+if uploaded_files:
+    uso = []
+    lista_notas = []
+    quantidade_notas = []
+    lista_datas = []
+    lista_Destinos = []
+   
+    destinos_info = []
+    for nota in uploaded_files:
+            xml_data = nota.read()
+            documento = xmltodict.parse(xml_data)
+            print(documento['nfeProc']['NFe']['infNFe']['ide']['dhEmi'][:10])
+            print(opcao_selecionada)
+            if str(documento['nfeProc']['NFe']['infNFe']['ide']['dhEmi'][:10]) == str(opcao_selecionada):
+                nome = nota.name
+                if documento not in lista_filtrada:
+                       lista_filtrada.append(documento)
+                else:
+                    continue
+    print(lista_filtrada)
+    for documento in lista_filtrada:
+                    st.write("Você carregou a nota:", documento['nfeProc']['NFe']['infNFe']['ide']['nNF'])
+                    try:
+                                        
+                                            lista_datas.append(nota)
+                                            dict_nota_fiscal = documento['nfeProc']['NFe']['infNFe']
+                                            numero_da_nota = dict_nota_fiscal['ide']['nNF']
+                                            cliente = dict_nota_fiscal['dest']['xNome']
+                                            valor_total = dict_nota_fiscal['total']['ICMSTot']['vNF']
+                                            destino = f'{dict_nota_fiscal["dest"]["enderDest"]["xBairro"]},{dict_nota_fiscal["dest"]["enderDest"]["xlgr"]},{dict_nota_fiscal["dest"]["enderDest"]["nro"]},{dict_nota_fiscal["dest"]["enderDest"]["xMun"]},{dict_nota_fiscal["dest"]["enderDest"]["UF"]}'
+                                            data_emit = documento['nfeProc']['NFe']['infNFe']['ide']['dhEmi'][:10]
+                                            lista_produtos = []
+                                            lista_Destinos.append(destino)
+                                            descricao_produto = documento['nfeProc']['NFe']['infNFe']['det']['prod']['cProd']
+                                            valor_produto = documento['nfeProc']['NFe']['infNFe']['det']['prod']['vProd']
+                                            item_nota = f'Item: {descricao_produto}  valor: {valor_produto}'
+                                            lista_produtos.append(item_nota)
+
+                                                
+
+                                            dict_nota = {
+                                                'Produtos': lista_produtos,
+                                                'Número da Nota': numero_da_nota,
+                                                'Cliente': cliente,
+                                                'Valor Total': valor_total,
+                                                'Destino': destino,
+                                                'Volumes':len(lista_produtos),
+                                                'Data de Emissão':data_emit,
+                                                'status':'Entrega não completa'
+                                            }
+                                            lista_notas.append(dict_nota)
+                    except:
+                                        lista_produtos = []
+                                        if isinstance(documento['nfeProc']['NFe']['infNFe']['det'], list):
+                                            # Iterar sobre os elementos 'det'
+                                            for item in documento['nfeProc']['NFe']['infNFe']['det']:
+                                                descricao_produto = item['prod']['cProd']
+                                                valor_prod = item['prod']['vProd']
+                                                elemento = f'Item:{descricao_produto}  valor:{valor_prod}'
+                                                lista_produtos.append(elemento)
+                                        else:
+                # Acessar diretamente o campo 'cProd'
+                                            descricao_produto = documento['nfeProc']['NFe']['infNFe']['det']['prod']['cProd']
+                                            valor_prod = documento['nfeProc']['NFe']['infNFe']['det']['prod']['vProd']
+                                            elemento = f'Item:{descricao_produto}  Valor:{valor_prod}'
+                                            lista_produtos.append(elemento)
+                                        lista_datas.append(nota)
+                                        dict_nota_fiscal = documento['nfeProc']['NFe']['infNFe']
+                                        numero_da_nota = dict_nota_fiscal['ide']['nNF']
+                                        cliente = dict_nota_fiscal['dest']['xNome']
+                                        valor_total = dict_nota_fiscal['total']['ICMSTot']['vNF']
+                                        destino = f'{dict_nota_fiscal["dest"]["enderDest"]["xBairro"]},{dict_nota_fiscal["dest"]["enderDest"]["xLgr"]},{dict_nota_fiscal["dest"]["enderDest"]["nro"]},{dict_nota_fiscal["dest"]["enderDest"]["xMun"]},{dict_nota_fiscal["dest"]["enderDest"]["UF"]}'
+                                        data_emit = documento['nfeProc']['NFe']['infNFe']['ide']['dhEmi'][:10]
+                                        lista_Destinos.append(destino)
+
+                                                
+
+                                        dict_nota = {
+                                                'Produtos': lista_produtos,
+                                                'Número da Nota': numero_da_nota,
+                                                'Cliente': cliente,
+                                                'Valor Total': valor_total,
+                                                'Destino': destino,
+                                                'Volumes':len(lista_produtos),
+                                                'Data de Emissão':data_emit,
+                                                'status':'Entrega não completa'
+                                            }
+                                        lista_notas.append(dict_nota)
+                    address = "Itupeva,sp"
+                    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                    params = {
+                                "address": address,
+                                "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                            }
+
+                    response = requests.get(base_url, params=params)
+                    data = response.json()
+                    if data["status"] == "OK":
+                                location = data["results"][0]["geometry"]["location"]
+                                lat_inicial = location["lat"]
+                                lon_inicial = location["lng"]
+                    ponto_partida = (lat_inicial, lon_inicial)
+                    ponto_partida_dict = {
+                        'destino': 'Itupeva, SP',
+                        'distancia': 0,
+                        'Número da nota': '',
+                        'volumes': '',
+                        'Duração': '',
+                        'coordenadas': ponto_partida,
+                        'coordenadas_google': f'{lat_inicial},{lon_inicial}',
+                        'cliente': ''
+                    }
+    destinos_info.append(ponto_partida_dict)
+    base_url2 = "https://www.google.com/maps/dir/"
+    for item in lista_Destinos:
+                                valor = item
+                                address2 = f"{valor}"
+                                base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                                params = {
+                                    "address": address2,
+                                    "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                }
+
+                                response = requests.get(base_url, params=params)
+                                data = response.json()
+                                if data["status"] == "OK":
+                                    location = data["results"][0]["geometry"]["location"]
+                                    lat_final2 = location["lat"]
+                                    lon_final2 = location["lng"]
+                                    if valor == 0:
+                                        pass
+                                    else:
+                                        url = 'https://maps.googleapis.com/maps/api/directions/json'
+                                        params = {
+                                                    'origin': f'Itupeva,sp',  # Origem
+                                                    'destination': f'{str(address2)}',  # Destino
+                                                    'key': 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Sua chave de API do Google Maps
+                                                }
+
+                                                # Fazendo a requisição GET
+                                        response = requests.get(url, params=params)
+                                        if response.status_code == 200:
+                                                    data = response.json()
+                                                    route = data['routes'][0]  # Primeira rota disponível
+                                                    distance = route['legs'][0]['distance']['text'][:4]
+                                                    distancia = float(distance.replace(' km', '').replace(',', '.'))
+                                                    duration = route['legs'][0]['duration']['text']
+                                                    destinos_info.append({
+                                                        'destino': valor,
+                                                        'distancia': distancia,
+                                                        'Duração': duration,
+                                                        'coordenadas':(lat_final2,lon_final2),
+                                                        'coordenadas_google': f'{lat_final2},{lon_final2}',
+                                                        'cliente':cliente,
+                                                    })
+    lista_pontos = []
+    print(destinos_info)
+    destinos_ordenados = sorted(destinos_info, key=lambda x: x['distancia'])
+    for destino in destinos_ordenados:        # Criar o DataFrame com todos os dados das notas
+                                local=destino['coordenadas_google']
+                                lista_pontos.append(local)
+    final = base_url2 + '/'.join(lista_pontos)
+    dict_nota['link'] = f'{final}'                    
+                            
+    for elemento in lista_notas:
+                                data_emissao = elemento['Data de Emissão']
+                                ref.child(data_emissao).push().set(elemento)
+    else:
+                        print('não confere')
+    st.warning('Processo Concluído')
+    st.write(f'''Total de nota:{len(lista_notas)}''')
+if page == 'Login':
+    st.switch_page('meu_app.py')
+elif page == 'Rotas e suporte':
+    st.switch_page('pages/rotas.py')
---
+++ juanstreamlit/pages/rotas.py
@@ -0,0 +1,195 @@
+import streamlit as st
+import requests
+import re
+from googletrans import Translator
+import folium
+import tkinter as tk    
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+from pathlib import Path
+import os
+import tkcalendar 
+from datetime import datetime
+import xmltodict
+import shutil
+import datetime
+import requests
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+from geopy.geocoders import Nominatim
+from time import sleep
+import pprint
+from st_circular_progress import CircularProgress
+import webbrowser
+from suporte_inteligente import ia
+css = """
+<style>
+.centered-image {
+    display: block;
+    margin: 0 auto;
+}
+</style>
+"""
+
+# Insere o CSS no aplicativo
+st.markdown(css, unsafe_allow_html=True)
+
+# Exibe a imagem centralizada
+st.image('https://calscycle.ca/newsite/wp-content/uploads/2022/05/Thule.png', width=500)
+selected_page = st.sidebar.selectbox("Escolha uma página", ["Rotas e suporte", "Login", "Home"])
+seletor  = st.radio("Opções", ["rota da nota", "rota de todas as notas", "suporte inteligente"])
+
+
+if selected_page == 'Login':
+    st.switch_page('meu_app.py')
+elif selected_page == 'Home':
+    st.switch_page('pages/home.py')
+
+if seletor == 'rota da nota':
+    pesquisa_nota = st.text_input(label='Pesquisar rota dessa nota')
+    pesquisa = st.button('ver rotas')
+    if pesquisa:
+        lista = []
+        requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+        roteiro = requiscao.json()
+        dados = roteiro['bancodedadosroteirooficial']
+        for item in dados:
+                roteiro = dados[f'{item}']
+                for elemento in roteiro:
+                    nota = roteiro[f'{elemento}']
+                    if nota['Número da Nota'] == str(pesquisa_nota):
+                        volumes = nota['Volumes']
+                        valor = nota['Valor Total']
+                        Cliente  = nota['Cliente']
+                        base_url2 = "https://www.google.com/maps/dir/"
+                        address = "Itupeva,sp"
+                        base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                        params = {
+                                    "address": address,
+                                    "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                }
+
+                        response = requests.get(base_url, params=params)
+                        data = response.json()
+                        if data["status"] == "OK":
+                                    location = data["results"][0]["geometry"]["location"]
+                                    lat_inicial = location["lat"]
+                                    lon_inicial = location["lng"]
+                        ponto_partida = (lat_inicial, lon_inicial)
+                        mapa = folium.Map(location=ponto_partida, zoom_start=15)
+                        ponto_partida_dict = {
+                            'destino': 'Itupeva, SP',
+                            'distancia': 0,
+                            'Número da nota': '',
+                            'volumes': '',
+                            'Duração': '',
+                            'coordenadas': ponto_partida,
+                            'coordenadas_google': f'{lat_inicial},{lon_inicial}',
+                            'cliente': ''
+                        }
+                        destinos_info = [ponto_partida_dict['coordenadas_google']]
+                        destino = nota['Destino']
+                        
+                        address = f"{destino}"
+                        base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                        params = {
+                                    "address": address,
+                                    "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                }
+
+                        response = requests.get(base_url, params=params)
+                        data = response.json()
+                        if data["status"] == "OK":
+                                    location = data["results"][0]["geometry"]["location"]
+                                    lat_inicial = location["lat"]
+                                    lon_inicial = location["lng"]
+                                    localizacao = f'{lat_inicial},{lon_inicial}'
+        destinos_info.append(localizacao)
+        final = base_url2 + '/'.join(destinos_info)
+        webbrowser.open(final)     
+        st.warning(f'Nota: {nota['Número da Nota']} volumes: {volumes} valor:{valor} cliente:{Cliente}')
+elif seletor == "rota de todas as notas":
+        lista_total = []
+        destinos_info = []
+        requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+        roteiro = requiscao.json()
+        dados = roteiro['bancodedadosroteirooficial']
+        base_url2 = "https://www.google.com/maps/dir/"
+        
+        for item in dados:
+                roteiro = dados[f'{item}']
+                lista_total.append(item)
+        opcao_selecionada = st.selectbox("Selecione uma data", lista_total)
+        total = st.button("Pesquisar todas as rotas")
+        if total:
+            st.write(f'Total de Roteiros disponíveis: {len(lista_total)}')
+            for item in dados:
+                if item == opcao_selecionada:
+                    roteiro = dados[f'{item}']
+                    for elemento in roteiro:
+                            nota = roteiro[f'{elemento}']
+                            volumes = nota['Volumes']
+                            valor = nota['Valor Total']
+                            Cliente  = nota['Cliente']
+                            
+                            address = "Itupeva,sp"
+                            base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                            params = {
+                                            "address": address,
+                                            "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                        }
+
+                            response = requests.get(base_url, params=params)
+                            data = response.json()
+                            if data["status"] == "OK":
+                                            location = data["results"][0]["geometry"]["location"]
+                                            lat_inicial = location["lat"]
+                                            lon_inicial = location["lng"]
+                            ponto_partida = (lat_inicial, lon_inicial)
+                            ponto_partida_dict = {
+                                    'destino': 'Itupeva, SP',
+                                    'distancia': 0,
+                                    'Número da nota': '',
+                                    'volumes': '',
+                                    'Duração': '',
+                                    'coordenadas': ponto_partida,
+                                    'coordenadas_google': f'{lat_inicial},{lon_inicial}',
+                                    'cliente': ''
+                                }
+                            destinos_info.append(ponto_partida_dict['coordenadas_google'])
+                            destino = nota['Destino']
+                                
+                            address = f"{destino}"
+                            base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+                            params = {
+                                            "address": address,
+                                            "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                        }
+
+                            response = requests.get(base_url, params=params)
+                            data = response.json()
+                            if data["status"] == "OK":
+                                            location = data["results"][0]["geometry"]["location"]
+                                            lat_inicial = location["lat"]
+                                            lon_inicial = location["lng"]
+                                            localizacao = f'{lat_inicial},{lon_inicial}'
+                                            destinos_info.append(localizacao)
+                final = base_url2 + '/'.join(destinos_info)
+                webbrowser.open(final) 
+elif seletor == "suporte inteligente":
+       texto_ia = st.text_input(label='Digite sua pergunta')
+       botao_ia = st.button('ok')
+       if botao_ia:
+              ia(pergunta=texto_ia)
+
+        
+              
+              
+
+            
+      
+
+    
+
+    
