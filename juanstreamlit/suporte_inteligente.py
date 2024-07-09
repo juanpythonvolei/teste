a4983376ec729ab5aae50464de23b62a701e3f1e
juanpythonvolei <juanzsalca@outlook.com>
2024-07-09 19:14:45 -0300

    Update suporte_inteligente.py

--- juanstreamlit/suporte_inteligente.py
+++ juanstreamlit/suporte_inteligente.py
@@ -23,8 +23,7 @@ def ia(pergunta):
     for item in dados:
         roteiro = dados[f'{item}']
         for elemento in roteiro:
-            nota = roteiro[f'{elemento}']
-            for item in nota:
+                nota = roteiro[f'{elemento}']
                 numero_nota = nota['Número da Nota']
                 destino = nota['Destino']
                 data_De_emissao = nota['Data de Emissão']
@@ -33,8 +32,8 @@ def ia(pergunta):
                 Produtos = nota['Produtos'][0]
                 status = nota['status']
                 valor  = nota['Valor Total']
-            infos = f'Numero nota:{numero_nota}. volumes:{volumes}. cliente:{cliente}. Produtos:{Produtos}. status:{status}. valor:{valor}. destino:{destino}. data de emissão:{data_De_emissao}\n'
-            texto_nota += str(infos)
+                infos = f'Numero nota:{numero_nota}. volumes:{volumes}. cliente:{cliente}. Produtos:{Produtos}. status:{status}. valor:{valor}. destino:{destino}. data de emissão:{data_De_emissao}\n'
+                texto_nota += str(infos)
     response = chat.send_message(f'{pergunta}:\n\n{texto_nota}\n')
     resposta = response.text
     st.write(f'{resposta}')
