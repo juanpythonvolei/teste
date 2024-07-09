e71135c3e9a17919bba1540e535e3c35bd697965
juanpythonvolei <juanzsalca@outlook.com>
2024-07-09 13:15:46 -0300

    Update meu_app.py

--- juanstreamlit/meu_app.py
+++ juanstreamlit/meu_app.py
@@ -53,7 +53,7 @@ css = """
 st.markdown(css, unsafe_allow_html=True)
 key = 'AIzaSyDKr5U-JLK2SvlndWbdNULNCCJNRYVv4rg'
 # Exibe a imagem centralizada
-st.image('https://calscycle.ca/newsite/wp-content/uploads/2022/05/Thule.png', width=500)
+st.image('https://calscycle.ca/newsite/wp-content/uploads/2022/05/Thule.png', width="100vw",heiht = '100vh')
 
 login = st.text_input(label='Digite seu usuário')
 
