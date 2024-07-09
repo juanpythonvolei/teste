b069c26f99cf398935ac2b612d78fb62f732be8e
juanpythonvolei <juanzsalca@outlook.com>
2024-07-08 21:50:24 -0300

    Update rotas.py

--- juanstreamlit/pages/rotas.py
+++ juanstreamlit/pages/rotas.py
@@ -99,9 +99,9 @@ if seletor == 'rota da nota':
                                     lon_final = location["lng"]
                                     localizacao = f'{lat_final},{lon_final}'
                                     destinos_info.append(localizacao)
-                    final = base_url2 + '/'.join(destinos_info)
-                    st.markdown(final)     
-                    st.warning(f'Nota: {numero} volumes: {volumes} valor:{valor} cliente:{Cliente}')
+                                    final = base_url2 + '/'.join(destinos_info)
+                                    st.markdown(final)     
+                                    st.warning(f'Nota: {numero} volumes: {volumes} valor:{valor} cliente:{Cliente}')
 elif seletor == "rota de todas as notas":
         lista_total = []
         destinos_info = []
