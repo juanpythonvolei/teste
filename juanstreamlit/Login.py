8c30c34fafe23b07e5c799636a4a097ee19d26e1
juanpythonvolei <juanzsalca@outlook.com>
2024-07-09 15:11:34 -0300

    Update Login.py

--- juanstreamlit/Login.py
+++ juanstreamlit/Login.py
@@ -3,8 +3,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore,db
 import requests
 import os
-st.set_page_config(initial_sidebar_state="collapsed")
-st.set_page_config(page_title="Roterização e suporte")
+st.set_page_config(initial_sidebar_state="collapsed",page_title="Roterização e suporte")
+
 
 st.markdown("""
     <style>
