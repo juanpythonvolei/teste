1adc6f0da94e14a37a3cc838775e14875d682a18
juanpythonvolei <juanzsalca@outlook.com>
2024-07-13 17:08:51 -0300

    Update Entregas.py

--- juanstreamlit/pages/Entregas.py
+++ juanstreamlit/pages/Entregas.py
@@ -29,7 +29,7 @@ css_style = """
       # Aplicando o estilo e inserindo os quadrados
 st.markdown(f"<style>{css_style}</style>", unsafe_allow_html=True)
 with col1:
-          st.markdown(f'<div class="my-square">Total Notas:{len(list(set(lista_conferida)))}</div>', unsafe_allow_html=True)
+          st.markdown(f'<div class="my-square">Total Notas:{len(lista_conferida)}</div>', unsafe_allow_html=True)
 with col2:
           st.markdown(f'<div class="my-square">Total Destinos</div>', unsafe_allow_html=True)
 with col3:
