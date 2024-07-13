7cda89a34663a4b6eec524b7b60721a3f47628ec
juanpythonvolei <juanzsalca@outlook.com>
2024-07-13 16:20:16 -0300

    Add files via upload

---
+++ juanstreamlit/pages/Entregas.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests
+
+lista_total = []
+destinos_info = []
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+dados = roteiro['bancodedadosroteirooficial']
+base_url2 = "https://www.google.com/maps/dir/"
+        
+for item in dados:
+                roteiro = dados[f'{item}']
+                lista_total.append(item)
+opcao_selecionada = st.selectbox("Selecione uma data", lista_total)
+total = st.button("Iniciar Nova Entrega")
+opcao_selecionada = st.checkbox("Marque esta opção")
+if total:
+        for item in dados:
+            roteiro = dados[f'{item}']
+            for elemento in roteiro:
+                nota = roteiro[f'{elemento}']
+                volumes = nota['Volumes']
+                numero_nota = nota['Número da Nota']
+                valor = nota['Valor Total']
+                cliente = nota['Cliente']
+            
