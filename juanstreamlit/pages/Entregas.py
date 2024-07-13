53c7f2a74848a974e323616b3348f51ed4a92711
juanpythonvolei <juanzsalca@outlook.com>
2024-07-13 17:46:51 -0300

    Update Entregas.py

--- juanstreamlit/pages/Entregas.py
+++ juanstreamlit/pages/Entregas.py
@@ -46,6 +46,8 @@ for nota, estado in checkbox_states.items():
                             link = f'https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/bancodedadosroteirooficial/{opcao_selecionada}/{elemento}/status.json'
                             data = '{"status": "Entrega realizada"}'
                             requests.patch(link, data=data)
+                            st.write(len(lista_conferida))  
+                            st.write(len(lista_notas))        
                         st.warning('Entrega realizada com Sucesso')
             else:
                         pass
