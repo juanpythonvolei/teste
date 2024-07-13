c7506163fd9ae4208cd7742b2288e3047e8876e7
juanpythonvolei <juanzsalca@outlook.com>
2024-07-13 16:53:07 -0300

    Update Entregas.py

--- juanstreamlit/pages/Entregas.py
+++ juanstreamlit/pages/Entregas.py
@@ -12,22 +12,23 @@ checkbox_states = {}
 # Exibe a seleção da data
 lista_total = [item for item in dados]
 opcao_selecionada_data = st.selectbox("Selecione uma data", lista_total)
-botao = st.button('Inicar Nova Entrega')
-# Exibe as notas com checkboxes
-if botao:
-    for item in dados:
-        roteiro = dados[item]
-        for elemento in roteiro:
-            nota = roteiro[elemento]
-            volumes = nota['Volumes']
-            numero_nota = nota['Número da Nota']
-            valor = nota['Valor Total']
-            cliente = nota['Cliente']
-            data = nota['Data de Emissão']
-            if data == opcao_selecionada_data:
-                # Usa o dicionário para controlar o estado da checkbox
-                checkbox_states[numero_nota] = st.checkbox(f"Cliente: {cliente}. Nota: {numero_nota}. Volumes: {volumes}", key=numero_nota)
 
-    for nota, estado in checkbox_states.items():
-        if estado:
-            st.write(f"Nota {nota} marcada")
+# Exibe as notas com checkboxes
+for item in dados:
+    roteiro = dados[item]
+    for elemento in roteiro:
+        nota = roteiro[elemento]
+        volumes = nota['Volumes']
+        numero_nota = nota['Número da Nota']
+        valor = nota['Valor Total']
+        cliente = nota['Cliente']
+        data = nota['Data de Emissão']
+        if data == opcao_selecionada_data:
+            # Usa o dicionário para controlar o estado da checkbox
+            checkbox_states[numero_nota] = st.checkbox(f"Cliente: {cliente}. Nota: {numero_nota}. Volumes: {volumes}", key=numero_nota)
+
+# Agora você pode usar o dicionário 'checkbox_states' conforme necessário
+st.write("Notas selecionadas:")
+for nota, estado in checkbox_states.items():
+    if estado:
+        st.write(f"Nota {nota} marcada")
