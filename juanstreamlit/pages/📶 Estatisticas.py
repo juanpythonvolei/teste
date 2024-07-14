9c7d05bebb06ee2970b38859cd341fd271c02b25
juanpythonvolei <juanzsalca@outlook.com>
2024-07-14 15:12:29 -0300

    Update 📶 Estatisticas.py

--- juanstreamlit/pages/📶 Estatisticas.py
+++ juanstreamlit/pages/📶 Estatisticas.py
@@ -41,10 +41,10 @@ try:
                           volumes = nota['Volumes']
                           cliente = nota['Cliente']
                           Produtos = nota['Produtos'][0]
-                          if nota['status']:
-                            status = nota['status']
-                          elif nota['status']['status']:
+                          if nota['status']['status']:
                             status = nota['status']['status']
+                          elif nota['status']:
+                            status = nota['status']
                           valor  = nota['Valor Total']
                       valor_total += float(valor)
                       texto_nota.append(destino)
