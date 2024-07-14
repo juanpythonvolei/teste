035417482f7f12f52b296ffac960ff62a02222cd
juanpythonvolei <juanzsalca@outlook.com>
2024-07-14 13:11:27 -0300

    Update Historico.py

--- juanstreamlit/pages/Historico.py
+++ juanstreamlit/pages/Historico.py
@@ -22,60 +22,66 @@ css_style = """
       # Aplicando o estilo e inserindo os quadrados
 if selected == 'Historico de Entregas':
           for item in dados:
-                      texto_historico = ''
-                      roteiro = dados[f'{item}']
-                      for elemento in roteiro:
-                              nota = roteiro[f'{elemento}']
-                              volumes = nota['Volumes']
-                              data = nota['Data de Emissão']    
-                              numero_nota = nota['Número da Nota']
-                              valor = nota['Valor Total']
-                              cliente = nota['Cliente']
-                              historico = f'''\n
-                              
-          Data: {data}\n
-          Volumes: {volumes}\n                
-          Numero: {numero_nota}\n
-          Valor: {valor}\n    
-          Cliente: {cliente}
-                              
-                              \n
-                              '''               
-                              texto_historico += historico
-                      st.markdown(f'<div class="my-square">{texto_historico}</div>', unsafe_allow_html=True)      
+                      try:               
+                                texto_historico = ''
+                                roteiro = dados[f'{item}']
+                                for elemento in roteiro:
+                                        nota = roteiro[f'{elemento}']
+                                        volumes = nota['Volumes']
+                                        data = nota['Data de Emissão']    
+                                        numero_nota = nota['Número da Nota']
+                                        valor = nota['Valor Total']
+                                        cliente = nota['Cliente']
+                                        historico = f'''\n
+                                        
+                    Data: {data}\n
+                    Volumes: {volumes}\n                
+                    Numero: {numero_nota}\n
+                    Valor: {valor}\n    
+                    Cliente: {cliente}
+                                        
+                                        \n
+                                        '''               
+                                        texto_historico += historico
+                                st.markdown(f'<div class="my-square">{texto_historico}</div>', unsafe_allow_html=True)      
+                    except:
+                              st.warning('Roteiro não está disponível')
 elif selected ==  "Produtos das entregas":
           lista_total = []
           destinos_info = []
           requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
           roteiro = requiscao.json()
           dados = roteiro['bancodedadosroteirooficial']
-          base_url2 = "https://www.google.com/maps/dir/"   
-          for item in dados:
-                          roteiro = dados[f'{item}']
-                          lista_total.append(item)
-          opcao_selecionada = st.selectbox("Selecione uma data", lista_total)
-          for item in dados:
-                      texto_historico = ''
-                      roteiro = dados[f'{item}']
-                      for elemento in roteiro:
-                              nota = roteiro[f'{elemento}']
-                              data = nota['Data de Emissão']    
-                              if data == opcao_selecionada:
-                                        volumes = nota['Volumes']
-                                        numero_nota = nota['Número da Nota']
-                                        valor = nota['Valor Total']
-                                        cliente = nota['Cliente']
-                                        produtos = nota['Produtos']
-                                        historico = f'''
-                                        
-          Data: {data}\n              
-          Numero: {numero_nota}\n
-          Produtos: {produtos}\n
-                                        
-                                        
-                                        '''               
-                                        texto_historico += historico
-                      st.markdown(f'<div class="my-square">{texto_historico}</div>', unsafe_allow_html=True)              
+          base_url2 = "https://www.google.com/maps/dir/"
+          try:
+                    for item in dados:
+                                    roteiro = dados[f'{item}']
+                                    lista_total.append(item)
+                    opcao_selecionada = st.selectbox("Selecione uma data", lista_total)
+                    for item in dados:
+                                texto_historico = ''
+                                roteiro = dados[f'{item}']
+                                for elemento in roteiro:
+                                        nota = roteiro[f'{elemento}']
+                                        data = nota['Data de Emissão']    
+                                        if data == opcao_selecionada:
+                                                  volumes = nota['Volumes']
+                                                  numero_nota = nota['Número da Nota']
+                                                  valor = nota['Valor Total']
+                                                  cliente = nota['Cliente']
+                                                  produtos = nota['Produtos']
+                                                  historico = f'''
+                                                  
+                    Data: {data}\n              
+                    Numero: {numero_nota}\n
+                    Produtos: {produtos}\n
+                                                  
+                                                  
+                                                  '''               
+                                                  texto_historico += historico
+                                st.markdown(f'<div class="my-square">{texto_historico}</div>', unsafe_allow_html=True)    
+          except:
+                    st.warnig('Roteiro não está disponível')
           
     
      
