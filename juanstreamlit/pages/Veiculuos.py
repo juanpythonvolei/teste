fba6ed4cf4a99f8110a559affe1a718025087b7d
juanpythonvolei <juanzsalca@outlook.com>
2024-07-16 22:45:00 -0300

    Update Veiculuos.py

--- juanstreamlit/pages/Veiculuos.py
+++ juanstreamlit/pages/Veiculuos.py
@@ -8,9 +8,15 @@ from firebase_admin import credentials, firestore,db
 seletor  = option_menu("Menu Principal", ["Cadastrar Veículos", "Pesquisar Veículos"], icons=["truck", "search"], default_index=1)
 ref = db.reference('Veículos')
 if seletor == 'Cadastrar Veículos':
+    lista_km = []
+    km = 0
+    for i in range(30):
+        km += 1
+        lista_km.append(km)
+if seletor == 'Cadastrar Veículos':
     Veículo = st.text_input(label='Digite o nome do Veículo')
     Placa = st.text_input(label='Digite a placa do veículo')
-    km = st.text_input(label='Digite a relação km/L do veículo')
+    km = st.selectbox("Selecione a relação km/L do veículo", lista_km)
     foto = st.text_input(label='Insira a url da foto do Veículo')
     botao = st.button('Cadastrar Veículo')
     dict_veiculo = {'nome':Veículo,
